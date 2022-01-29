c963d950e9556a48e1ad817e6833929942b84e9d
609529897 <609529897@qq.com>
2022-01-29 13:59:20 +0800

    day2

--- day1/hello_cargo/target/rls/debug/deps/hello_cargo-534ac33fc82e1743.d
+++ day1/hello_cargo/target/rls/debug/deps/hello_cargo-534ac33fc82e1743.d
@@ -1,5 +1,5 @@
-/Users/ningnarigele/Desktop/rs/day1/sec2/hello_cargo/target/rls/debug/deps/hello_cargo-534ac33fc82e1743.rmeta: src/main.rs
+/Users/ningnarigele/Desktop/rs/day1/hello_cargo/target/rls/debug/deps/hello_cargo-534ac33fc82e1743.rmeta: src/main.rs
 
-/Users/ningnarigele/Desktop/rs/day1/sec2/hello_cargo/target/rls/debug/deps/hello_cargo-534ac33fc82e1743.d: src/main.rs
+/Users/ningnarigele/Desktop/rs/day1/hello_cargo/target/rls/debug/deps/hello_cargo-534ac33fc82e1743.d: src/main.rs
 
 src/main.rs:
